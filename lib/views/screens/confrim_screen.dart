7fea99255f23d6c6101d310f34307152d5f4a801
ovezthaking <oliwerx12@gmail.com>
2025-04-25 01:20:02 +0200

    Add video Screen

---
+++ lib/views/screens/confrim_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConfrimScreen extends StatelessWidget {
+  const ConfrimScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
