b6e2d30083a6cac260f4f55f3854c0579d190e0c
ovezthaking <oliwerx12@gmail.com>
2025-05-26 15:18:37 +0200

    clean

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,24 +3,24 @@ import 'package:http/http.dart' as http;
 import 'package:mufreak/api_consts.dart';
 
 
-class ApiService {
-  static Future<void> getModels() async{
-    try {
-       var response = await http.post(
-        Uri.parse(BASE_URL),
-        headers: {
-          'Content-Type' : 'application/json',
-          },
+// class ApiService {
+//   static Future<void> getModels() async{
+//     try {
+//        var response = await http.post(
+//         Uri.parse(BASE_URL),
+//         headers: {
+//           'Content-Type' : 'application/json',
+//           },
         
-        );
+//         );
 
-        Map jsonResponse = jsonDecode(response.body);
-        print("jsonResponse $jsonResponse"); 
-    } catch(e){
-      print("error $e");
-    }
-  }
-}
+//         Map jsonResponse = jsonDecode(response.body);
+//         print("jsonResponse $jsonResponse"); 
+//     } catch(e){
+//       print("error $e");
+//     }
+//   }
+// }
 
 Future<String> getOpenRouterResponse(String userInput) async {
     var endpoint = "$BASE_URL_OPENR/chat/completions";
